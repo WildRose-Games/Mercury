485eb9b86c5f9b731d1029fa43a0d38a0d8c07c3
Gwenivere Benzschawel <wildrosegamedev@gmail.com>
2024-06-07 14:44:27 -0500

    added table of contents

--- src/Hg/Hg.h
+++ src/Hg/Hg.h
@@ -3,28 +3,63 @@
  *  Copyright: 2024
  *  License: MIT
  *
- *  Purpose: Hg.h is the documentation for using the mercury engine.
+ *  Purpose: Hg.h is the documentation for using the Mercury engine.
  *  It's also the header file for connecting the game translation unit, and
  *  the engine translation unit.
+ */
+
+/*
+ *  TABLE OF CONTENTS     (00.00)
+ *  
+ *  Usefull Macro Defines and Includes      (01.00)
+ *    Standard Library Includes           (01.01)
+ *    Mercury Engine Version              (01.02)
+ *    Shared Object (DLL) Filename Defs   (01.03)    
+ *    Window Initial Size                 (01.04)
+ *    Max Log or Filepath Length          (01.05)
+ *    Math Functions (cglm)               (01.06)
+ *
+ *  Mercury Engine            (02.00)
+ *    Input                 (02.01)
+ *    File IO               (02.02)
+ *    Mesh                  (02.03)
+ *    Camera                (02.04)
+ *    Entity                (02.05)
+ *    Light                 (02.06)
+ *    Load Wavefront obj    (02.07)
+ *    Shader                (02.08)
+ *    Vertex Buffer         (02.09)
+ *    Textures              (02.10)
+ *    Rendering             (02.11)
+ *  
+ *  game.h    (03.00)
+ */
+
+/*
+ * Use the number keys "(0X.XX)" in the table of contents
+ * to find the section with a search
  */
 
 #ifndef HG_H
 #define HG_H
 
-/**************************************************************************
- *                   Basic helper macros and includes                     *
- **************************************************************************/
+/***************************************************************************
+ *  Basic helper macros and includes                              (01.00)  *
+ ***************************************************************************/
 
+
+/* Standard Library Includes (01.01) */
 #include <stdbool.h>
 #include <stdint.h>
 #include <math.h>
+#include <time.h>
 
-//Version
+/* Mercury Engine Version (01.02) */
 #define HG_MAJOR 0
 #define HG_MINOR 2
 #define HG_PATCH 0
 
-//Shared Object
+/* Shared Object (DLL) Filename Defs (01.03) */
 #ifdef HG_BUILD_DEBUG
 #define SO_FILE "./libGameDbg.so"
 #define SO_FILE_TEMP "./libGameDbg_temp.so"
@@ -35,26 +70,24 @@
 #define SO_FILE_TEMP "./libGame_temp.so"
 #define SO_FILENAME "libGame.so"
 #define SO_FILENAME_TEMP "libGame_temp.so"
-#endif //HG_BUILD_DEBUG
+#endif /* HG_BUILD_DEBUG */
 
-//Window
+/* Window Initial Size (01.04) */
 #define INIT_WIDTH 640
 #define INIT_HEIGHT 480
-#define INIT_ASPECT_RATIO (float)INIT_WIDTH / (float)INIT_HEIGHT
 
-//Log
+/* Max Log or Filepath Length (01.05) */
+#include "HgL_Log.h"
 #define LOG_LENGTH 1028
-
-//File
 #define PATH_LENGTH 512
 
-//camera
-#define CAMERA_FOV (90 * M_PI / 180.0)
-#define CAMERA_NEAR 0.1
-#define CAMERA_FAR 100
-
-//memory allocation
+/*memory allocation
+ * TODO move this somewhere else, users should not need to modify this file.
+ */
+#include "HgL_Arena.h"
 #define ALLOC_MEM_SIZE GIGABYTES(1)
+
+/* Math Functions (cglm) (01.06) */
 #include <cglm/cglm.h>
 
 #ifndef M_PI
@@ -71,18 +104,10 @@
 #define MAX(i, j) (((i) > (j)) ? (i) : (j))
 
 /**************************************************************************
- *                                  Engine                                *
+ *  Mercury Game Engine                                          (02.00)  *
  **************************************************************************/
 
-/*******
- * Log *
- *******/
-
-#include "HgL_Log.h"
-
-/*********
- * Input *
- *********/
+/* Input (02.01) */
 typedef struct HgButtonInput{
   bool isStartDown;
   bool isEndDown;
@@ -112,32 +137,31 @@ typedef struct HgInput{
   HgStickInput rightStick;
 }HgInput;
 
-/**********
- * Memory *
- **********/
+/* File IO (02.02) */
+size_t hgGetFileSize(const char* fileLoc);
 
-#include "HgL_Arena.h"
+void hgGetFileStr(char* buffer, size_t size, const char* fileLoc);
 
-/********
- * Mesh *
- ********/
+void hgDeleteFile(const char* file);
+
+void hgCopyFile(const char* src, const char* dest);
+
+time_t hgFileModTime(const char* file);
+
+/* Mesh (02.03) */
 #define MESH_SHADER_FILE "mesh"
 
 typedef struct HgMesh HgMesh;
 
 void hgCleanupMesh(HgArena *hgArena, HgMesh *hgMesh);
 
-/**********
- * Camera *
- **********/
+/* Camera (02.04) */
 typedef struct HgCamera {
   mat4 proj;
   mat4 view;
 }HgCamera;
 
-/**********
- * Entity *
- **********/
+/* Entity (02.05) */
 typedef struct HgEntity{
   mat4 trans;
   HgMesh *mesh;
@@ -146,9 +170,14 @@ typedef struct HgEntity{
 
 void hgInitEntity(HgEntity *hgEntity, HgMesh *hgMesh);
 
-/***********
- * ObjLoad *
- ***********/
+/* Lights and Lighting (02.06)*/
+typedef struct HgLight {
+  vec3 position;
+  vec3 color;
+  vec3 ambient;
+}HgLight;
+
+/* Load Wavefront obj (02.07) */
 HgMesh* hgLoadObjMesh(HgArena *hgArena,
                       const char* file,
                       const char* object);
@@ -156,13 +185,7 @@ HgMesh* hgLoadObjMesh(HgArena *hgArena,
 #define hgLoadMesh(a, b) hgLoadObjMesh(a, b, b)
 
 
-/**************************************************************************
- *                                  Platform                              *
- **************************************************************************/
-
-/**********
- * Shader *
- **********/
+/* Shader (02.08) */
 typedef struct HgShader HgShader;
 
 HgShader hgCreateShader(HgArena *hgArena, const char *file);
@@ -176,9 +199,7 @@ void hgUnbindShader(void);
 void hgCleanupShader(HgArena *hgArena, HgShader *s);
 #include "uniform.h"
 
-/****************
- * VertexBuffer *
- ****************/
+/* Vertex Buffer (02.09) */
 typedef struct HgVertexBuffer HgVertexBuffer;
 
 typedef struct HgVertex {
@@ -200,9 +221,7 @@ void hgUnbindVertexBuffer(void);
 
 void hgCleanupVertexBuffer(HgVertexBuffer *vb);
 
-/***********
- * Texture *
- ***********/
+/* Textures (02.10) */
 typedef struct HgTexture HgTexture;
 
 void hgBindTexture(HgTexture *t, uint32_t slot);
@@ -213,19 +232,9 @@ void hgLoadMeshTexture(HgMesh *hgMesh, char* file);
 
 void hgCleanupTexture(HgTexture *t);
 
-/*********
- * Light *
- *********/
-typedef struct HgLight {
-  vec3 position;
-  vec3 color;
-  vec3 ambient;
-}HgLight;
 
 
-/************
- * Renderer *
- ************/
+/* Rendering (02.11) */
 void hgBeginDraw(void);
 
 void hgDrawMesh(HgEntity *hgEntity, HgLight *hgLight, HgCamera *hgCamera);
@@ -233,6 +242,6 @@ void hgDrawMesh(HgEntity *hgEntity, HgLight *hgLight, HgCamera *hgCamera);
 
 /**************************************************************************/
                                 #include "game.h"
-/**************************************************************************/
+/***************************************************************(03.00)****/
 
 #endif //HG_H
